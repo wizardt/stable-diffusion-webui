041f5198a2b9e9c5c8b25612f2d8018850e458a6
主席 <wizard_t1987@126.com>
2023-01-29 21:55:26 +0800

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--ckpt-dir E:\PYTRADING\NOVELAI-materials\ckpt --enable-console-prompts --xformers --opt-split-attention --lowvram --theme dark 
 
 call webui.bat
